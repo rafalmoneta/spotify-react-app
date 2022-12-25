3aa6d699d45cba80b8293437fba50fa008a1cf8a
rafalmoneta <rafalmoneta5@gmail.com>
2022-12-25 19:07:43 +0100

    github action fix

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,40 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { getPlaylistInfo} from '../../spotify/spotify';
-import './Playlist.scss'
-import Track from '../Track/Track';
-import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getPlaylistInfo } from "../../spotify/spotify";
+import "./Playlist.scss";
+import Track from "../Track/Track";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
 const Playlist = () => {
   const [playlist, setPlaylist] = useState(null);
   const [tracks, setTracks] = useState(null);
-  let {playlistID} = useParams();
-  
-  useEffect(()=>{
+  let { playlistID } = useParams();
+
+  useEffect(() => {
     let id = playlistID.slice(1);
     const fetchData = async () => {
-      const {data} = await getPlaylistInfo(id);
+      const { data } = await getPlaylistInfo(id);
       setPlaylist(data);
       setTracks(data.tracks.items);
-    }
+    };
 
     fetchData();
-  }, [])
+  }, [playlistID]);
 
   return (
     <div>
-      {!playlist && 
-        <LoadingSpinner />
-      }
+      {!playlist && <LoadingSpinner />}
       <div className="playlist-wrapper">
-        {playlist && 
+        {playlist && (
           <div className="playlist-top">
             <div className="playlist-image">
-              <img src={playlist.images[0].url} alt="playlist "/>
+              <img src={playlist.images[0].url} alt="playlist " />
             </div>
             <div className="playlist-info">
               <h1>{playlist.name}</h1>
-              <span className="playlist-author">by {playlist.owner.display_name}</span>
+              <span className="playlist-author">
+                by {playlist.owner.display_name}
+              </span>
               <p>{playlist.description}</p>
               <div className="playlist-stats">
                 <div className="info">
@@ -52,30 +52,34 @@ const Playlist = () => {
               </div>
             </div>
           </div>
-        }
-        { tracks && 
+        )}
+        {tracks && (
           <div className="playlist-tracks">
             <h2>Tracks</h2>
             {tracks.map((track, index) => {
-              if(track.track) {
+              if (track.track) {
                 return (
                   <Track
-                    url={track.track.album ? track.track.album.images.slice(-1).pop().url : 'https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png'}
+                    url={
+                      track.track.album
+                        ? track.track.album.images.slice(-1).pop().url
+                        : "https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png"
+                    }
                     track={track.track.name}
                     artists={track.track.artists}
                     album={track.track.album.name}
                     duration={track.track.duration_ms}
                     key={index}
                   />
-                )
+                );
               }
+              return null;
             })}
           </div>
-        }
+        )}
       </div>
     </div>
   );
-}
- 
-export default Playlist;
+};
 
+export default Playlist;
